f0b97cc4ce0e2c86d2fa5142dfda7da950e10f7b
levi549 <cartaginezzilevi@gmail.com>
2026-05-28 10:48:52 -0300

    Add Botao component with button and styles

--- src/components/Botao.js
+++ src/components/Botao.js
@@ -1,1 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
 
+export default function Botao({texto, onPress}){
+
+    return(
+        <TouchableOpacity style={styles.button} onPress={onPress} activeOpacity={0.7}>
+            <Text style={styles.buttonText}>{texto}</Text>
+        </TouchableOpacity>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    button:{
+    backgroundColor: '#2187db',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 20,
+    marginBottom:10
+    },
+    buttonText:{
+    color: '#050505',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+})
